566a9cb6d83db6f68874e3fb7a9033f637bdc946
imaun <imun22@gmail.com>
2023-01-08 00:29:01 +0330

    Add ContentCategoryViewModel

---
+++ src/Behlog.Web.Models/ContentCategory/ContentCategoryViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Behlog.Web.Models;
+
+public class ContentCategoryViewModel
+{
+    public Guid Id { get; set; }
+    public Guid WebsiteId { get; set; }
+    public string Title { get; set; }
+    public string? AltTitle { get; set; }
+    public string Slug { get; set; }
+    public Guid LangId { get; set; }
+    public string LangCode { get; set; }
+    public Guid? ParentId { get; set; }
+    public string? Description { get; set; }
+    public Guid? ContentTypeId { get; set; }
+    public string ContentTypeName { get; set; }
+    public string ContentTypeTitle { get; set; }
+    public EntityStatusEnum Status { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CreatedDateDisplay => CreatedDate.ToLocalTime().ToPersianDateTextify();
+    public DateTime? LastUpdated { get; set; }
+    public string? LastUpdatedDisplay => LastUpdated?.ToLocalTime().ToPersianDateTextify();
+    public string? CreatedByUserId { get; set; }
+    public string? CreatedByUserName { get; set; }
+    public string? CreatedByUserDisplayName { get; set; }
+    public string? LastUpdatedByUserId { get; set; }
+    public string? LastUpdatedByUserName { get; set; }
+    public string? LastUpdatedByUserDisplayName { get; set; }
+    public string? CreatedByIp { get; set; }
+    public string? LastUpdatedByIp { get; set; }
+    public DateTime? LastStatusChangedOn { get; set; }
+    public string? LastStatusChangedOnDisplay => LastStatusChangedOn?.ToLocalTime().ToPersianDateTextify();
+}
--- src/Behlog.Web.Models/GlobalUsings.cs
+++ src/Behlog.Web.Models/GlobalUsings.cs
@@ -1,4 +1,4 @@
 global using Behlog.Core;
 global using Behlog.Core.Models;
 global using System.ComponentModel.DataAnnotations;
- 
+global using DNTPersianUtils.Core;
