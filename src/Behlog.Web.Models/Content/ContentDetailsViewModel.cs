d94a44a69ed018f1c80b523ecc1e6842f594a2bd
imaun <imun22@gmail.com>
2023-01-08 01:04:14 +0330

    Add ContentDetailsViewModel

---
+++ src/Behlog.Web.Models/Content/ContentDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Behlog.Web.Models;
+
+public class ContentDetailsViewModel
+{
+    public ContentDetailsViewModel() {
+        Categories = new List<ContentCategoryViewModel>();
+        RelatedContents = new List<ContentViewModel>();
+    }
+
+    public ContentViewModel Content { get; set; }
+    public ICollection<ContentCategoryViewModel> Categories { get; set; }
+    public ICollection<ContentViewModel> RelatedContents { get; set; }
+}
