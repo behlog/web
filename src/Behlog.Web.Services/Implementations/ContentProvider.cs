7f53c201bdd8708be2d981685add2da99f4281f9
imaun <imun22@gmail.com>
2023-01-08 23:10:37 +0330

    Add ContentProvider service

---
+++ src/Behlog.Web.Services/Implementations/ContentProvider.cs
@@ -0,0 +1,37 @@
+﻿namespace Behlog.Web.Services;
+
+/// <inheritdoc />
+public class ContentProvider : IContentProvider
+{
+    private readonly IBehlogMediator _behlog;
+
+    public ContentProvider(IBehlogMediator behlog) {
+        _behlog = behlog ?? throw new ArgumentNullException(nameof(behlog));
+    }
+
+    public async Task<ContentDetailsViewModel> GetDetailsAsync(
+        Guid websiteId, string contentTypeName, string slug, string langCode) {
+        websiteId.ThrowIfGuidIsEmpty(new BehlogInvalidEntityIdException(nameof(Website)));
+        if (contentTypeName.IsNotNullOrEmpty())
+            throw new ArgumentNullException(nameof(contentTypeName));
+        if (slug.IsNotNullOrEmpty())
+            throw new ArgumentNullException(nameof(slug));
+
+        var langId = BehlogSupportedLanguages.GetIdByCode(langCode);
+        var model = new ContentDetailsViewModel();
+        
+        var content = await _behlog.PublishAsync(
+            new QueryContentByContentTypeAndSlug(websiteId, slug, null, contentTypeName, langId));
+        content.ThrowExceptionIfReferenceIsNull(nameof(content));
+
+        var categories = await _behlog.PublishAsync(
+            new QueryContentCategoryByContentType(null, contentTypeName, langId));
+        model.Categories = categories?.Select(_ => new ContentCategoryViewModel(_)).ToList();
+
+        var latestContents = await _behlog.PublishAsync(
+            new QueryLatestContentsByContentType(websiteId, null, contentTypeName, 4));
+        model.RelatedContents = latestContents?.Select(_ => new ContentViewModel(_)).ToList();
+
+        return await Task.FromResult(model);
+    }
+}
