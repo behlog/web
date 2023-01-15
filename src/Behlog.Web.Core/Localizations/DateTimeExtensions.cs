40a3139f08ad919fa593847b0dbe81c055700b46
imun <imun22@gmail.com>
2023-01-15 12:11:43 +0330

    Add ToLocalDisplay for DateTime

--- src/Behlog.Web.Core/Localizations/DateTimeExtensions.cs
+++ src/Behlog.Web.Core/Localizations/DateTimeExtensions.cs
@@ -1,4 +1,6 @@
+using Behlog.Cms;
 using Behlog.Cms.Domain;
+using Behlog.Core;
 using Behlog.Extensions;
 using Language = Behlog.Cms.Domain.Language;
 
@@ -10,7 +12,40 @@ public static class DateTimeExtensions
     public static string ToLocalDisplay(this DateTime date, Language language)
     {
         language.ThrowExceptionIfArgumentIsNull(nameof(language));
-
-        return "";
+        if (language.Code == PersianLanguage.Code)
+        {
+            return date.ToPersianDateTimeText();
+        }
+        
+        return date.ToLongTimeString();
     }
+
+
+    public static string ToLocalDisplay(this DateTime date, Guid langId)
+    {
+        langId.ThrowIfGuidIsEmpty(new BehlogInvalidEntityIdException(nameof(Language)));
+
+        if (langId == PersianLanguage.Id)
+        {
+            return date.ToPersianDateTimeText();
+        }
+
+        return date.ToLocalTime().ToLongTimeString();
+    }
+
+    public static string ToLocalDisplay(this DateTime date, string langCode)
+    {
+        if (langCode.IsNullOrEmptySpace())
+            throw new ArgumentNullException(nameof(langCode));
+
+        if (langCode.ToUpper() == PersianLanguage.Code.ToUpper())
+        {
+            return date.ToPersianDateTimeText();
+        }
+
+        return date.ToLocalTime().ToLongTimeString();
+    }
+    
+    
+    
 }
