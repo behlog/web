3034d6db127f4e1cac8b69b745ce44b635ea6bdb
imaun <imun22@gmail.com>
2023-02-14 23:55:23 +0330

    Rename BodyTypeEnum to BodyType

--- src/Behlog.Web.Models/Comments/CommentViewModel.cs
+++ src/Behlog.Web.Models/Comments/CommentViewModel.cs
@@ -6,7 +6,7 @@ public class CommentViewModel
 {
     public string? Title { get; protected set; }
     public string Body { get; protected set; }
-    public ContentBodyTypeEnum BodyType { get; protected set; }
+    public ContentBodyType BodyType { get; protected set; }
     public CommentStatusEnum Status { get; protected set; }
     public string? Email { get; protected set; }
     public string? WebUrl { get; protected set; }
